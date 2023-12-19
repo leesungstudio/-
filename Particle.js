86e0c665af15627e11e7bf96105e5331d538d5ac
leesungstudio <47140533+SungWonStuDio@users.noreply.github.com>
2023-12-19 13:54:59 +0900

    1번쨰 표현

---
+++ Particle.js
@@ -0,0 +1,32 @@
+class Particle {
+    constructor(aColor) {
+      this.pos = createVector(random(0, width), random(0, height));
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.w = 4;
+      
+      this.c = aColor;
+    }
+    
+    addForce(aForce) {
+      this.acc.add(aForce);
+    }
+    
+    update() {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      
+      this.acc = createVector(0, 0);
+      this.vel.mult(0.9);
+      
+      if (this.pos.x < 0) this.pos.x = width;
+      if (this.pos.x > width) this.pos.x = 0;
+      if (this.pos.y < 0) this.pos.y = height;
+      if (this.pos.y > height) this.pos.y = 0;
+    }
+    
+    show() {
+      fill(this.c);
+      circle(this.pos.x, this.pos.y, this.w);
+    }
+  }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,66 @@
+let particles = [];
+let rPs = [];
+let yPs = [];
+let bPs = [];
+
+
 function setup() {
   createCanvas(400, 400);
+  noStroke();
+  
+  rPs = createParticles(color(250, 0, 0), 100); 
+  yPs = createParticles(color(250, 250, 0), 800);
+  bPs = createParticles(color(0, 0, 250), 10);
 }
 
+
 function draw() {
-  background(220);
+  background(0);
+  
+  rule(rPs, rPs, -0.2);
+  rule(yPs, rPs, 0.9);
+  rule(bPs, rPs, -0.7);
+  
+  rule(rPs, rPs, -0.9);
+  rule(yPs, rPs, 0.9);
+  
+  rule(rPs, rPs, 0.9);
+  rule(bPs, rPs, -0.9);
+  
+  rule(yPs, bPs, -0.9);
+  rule(bPs, yPs, -0.9);
+  
+  for (let i=0; i<particles.length; i++) {
+    let p = particles[i];
+    p.update();
+    p.show();
+  }
 }
+
+
+function rule(aPs, bPs, g) {
+  for (let i=0; i<aPs.length; i++) {    
+    for (let j=0; j<bPs.length; j++) {
+      let d = p5.Vector.dist(aPs[i].pos, bPs[j].pos);
+      if (d > 0.2 && d < 90) {
+        let force = p5.Vector.sub(aPs[i].pos, bPs[j].pos);
+        force.mult(g);
+        force.div(d*d);
+        aPs[i].addForce(force);
+      }
+    }
+  }
+}
+
+
+function createParticles(aColor, number) {
+  let group = [];
+  for (let i=0; i<number; i++) {
+    let p = new Particle(aColor);
+    group.push(p);
+    particles.push(p);
+  }
+  return group;
+}
+
+
